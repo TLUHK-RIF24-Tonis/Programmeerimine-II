fe4fb3c3e056f0455fad40d0305690e64aec7779
Tõnis P <putsepp.tonis@gmail.com>
2025-11-23 23:18:32 +0200

    Added: database integration to create course service

--- API_Development/code/src/courses/coursesService.ts
+++ API_Development/code/src/courses/coursesService.ts
@@ -1,7 +1,7 @@
 import { courses } from "../data";
 import ICourses from "./coursesInterface";
 import pool from "../database";
-import { FieldPacket } from "mysql2";
+import { FieldPacket, ResultSetHeader } from "mysql2";
 
 const getCourseById = async (id: number): Promise<ICourses | undefined> => {
     const [course]: [ICourses[], FieldPacket[]] = await pool.query(
@@ -15,23 +15,14 @@ const getAllCourses = async () : Promise<ICourses[]> => {
     return courses;
 };
 
-const createCourse = ( name: string, location: string, holes: number, par: number ) => {
-    const id = courses[courses.length - 1].id + 1;
+const createCourse = async ( name: string, location: string, holes: number, par: number ) => {
 
-    const allCourses = getAllCourses();
-    const exist = allCourses.some(c => c.name === name && c.location === location);
+    const sql = `
+    INSERT INTO courses ( course_name, location, holes, par ) VALUES (?, ?, ?, ?);
+    `
 
-    if ( exist ) return null;
-
-    const course: ICourses = {
-        id,
-        name,
-        location,
-        holes,
-        par
-    };
-    courses.push(course);
-    return course
+    const [addedCourse] = await pool.execute<ResultSetHeader>(sql, [name, location, holes, par]);
+    return addedCourse;
 };
 
 export default { getCourseById, getAllCourses, createCourse };
