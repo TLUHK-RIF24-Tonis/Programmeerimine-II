6678bfc30decf4303266e2fd4ef5bbbb72718035
Tõnis P <putsepp.tonis@gmail.com>
2025-11-26 00:36:30 +0200

    feat: coursesController error handling

--- API_Development/code/src/courses/coursesController.ts
+++ API_Development/code/src/courses/coursesController.ts
@@ -1,130 +1,127 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import coursesService from './coursesService';
+import CustomError from '../general/CustomError';
 
-const getAllCourses = async (req: Request, res: Response) => {
-
-const courses = await coursesService.getAllCourses();
-
-if (!courses) {
-    return res.status(200).json({
-        success: true,
-        message: 'No courses found!',
-        courses: []
-    })
-};
-
-return res.status(200).json({
-    success: true,
-    message: 'Courses loaded!',
-    courses
-});
-};
-
-const getCourseById = async (req: Request, res: Response) => {
-    
-    const id = Number(req.params.id);
-
-    const course = await coursesService.getCourseById(id);
-
-    if (!course) {
-        return res.status(404).json ({
-            success: false,
-            message: `Course with this id: ${id} does not exist!`
-        });
-    };
-
-    return res.status(200).json ({
-        success: true,
-        message: `Course with id: ${id} found!`,
-        course,
-    });
-};
-
-const createCourse = async ( req: Request, res: Response ) => {
-    const { name, location, holes, par } = req.body;
-
-    if ( !name || !location ) {
-        return res.status(400).json ({
-            success: false,
-            message: 'Course name or location can not be empty'
-        })
-    }
-    if ( holes == null || par == null) {
-        return res.status(400).json ({
-            success: false,
-            message: 'Course must have hole number and course PAR'
-        })
-    }
-
+const getAllCourses = async (req: Request, res: Response, next: NextFunction) => {
     try {
-    const course = await coursesService.createCourse( name, location, holes, par )
-    return res.status(201).json({
-        success: true,
-        id: course.insertId,
-        message: `Course created!`,
-        name,
-        location,
-        holes,
-        par
-    })} catch ( err: any ) {
-        if ( err.code === "ER_DUP_ENTRY") {
-            return res.status(409).json({
+        const courses = await coursesService.getAllCourses();
+
+        if (!courses) {
+            return res.status(200).json({
+                success: true,
+                message: 'No courses found!',
+                courses: []
+            })
+        };
+
+        return res.status(200).json({
+            success: true,
+            message: 'Courses loaded!',
+            courses
+        });
+    } catch ( error ) {
+        return next(error);
+    }
+};
+
+const getCourseById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const id = Number(req.params.id);
+
+        const course = await coursesService.getCourseById(id);
+
+        if (!course) {
+            throw new CustomError(`Atleast one field must be provided`, 400);
+            return res.status(404).json ({
                 success: false,
-                message: 'Course already exists!'
+                message: `Course with this id: ${id} does not exist!`
             });
+        };
+
+        return res.status(200).json ({
+            success: true,
+            message: `Course with id: ${id} found!`,
+            course,
+        });
+    } catch ( error ) {
+        return next(error);
+    }
+
+};
+
+const createCourse = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { name, location, holes, par } = req.body;
+
+        if ( !name || !location ) {
+            throw new CustomError(`Course name or location can not be empty`, 400);
         }
-        return res.status(500).json({
-            success: false,
-            message: "Internal server error"
-    })
+        if ( holes == null || par == null) {
+            throw new CustomError(`Course must have hole number and course PAR`, 400);
+        }
+
+        const course = await coursesService.createCourse( name, location, holes, par )
+        return res.status(201).json({
+            success: true,
+            id: course.insertId,
+            message: `Course created!`,
+            name,
+            location,
+            holes,
+            par
+        })} catch ( error ) {
+            if ((error as any).code === 'ER_DUP_ENTRY') {
+                return next(new CustomError("Course already exists!", 409));
+            }
+
+        return next(error);
     }
 };
 
-const deleteCourse = async ( req: Request, res: Response ) => {
-    const id = Number( req.params.id );
+const deleteCourse = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const id = Number( req.params.id );
 
-    const deleted = await coursesService.deleteCourse( id );
+        const deleted = await coursesService.deleteCourse( id );
 
-    if ( !deleted ) {
-        return res.status(404).json({
-            success: false,
-            message: `Course(${id}) not found!`
-        });
+        if ( !deleted ) {
+            throw new CustomError(`Course(${id}) not found!`, 404);
+        }
+
+        return res.status(204).send();
+    } catch ( error ) {
+        return next(error);
     }
-
-    return res.status(204).send();
 };
 
-const updatedCourse = async ( req: Request, res: Response ) => {
-    const id = Number( req.params.id );
-    const { course_name, course_location, holes, par } = req.body;
+const updatedCourse = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const id = Number( req.params.id );
+        const { course_name, course_location, holes, par } = req.body;
 
-    if ( 
-        course_name === undefined &&
-        course_location === undefined &&
-        holes === undefined &&
-        par === undefined 
-    ) {
-        return res.status(400).json({
-            success: false,
-            message: `Missing input: Course name, -Location, hole number or PAR`
+        if ( 
+            course_name === undefined &&
+            course_location === undefined &&
+            holes === undefined &&
+            par === undefined 
+        ) {
+            throw new CustomError(`Missing input: Course name, -Location, hole number or PAR`, 400);
+        }
+
+        const updated = await coursesService.updateCourse ( id, { course_name, course_location, holes, par } );
+
+        if ( !updated ) {
+            throw new CustomError(`Course(${id}) does not exist!`, 404);
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: `Course updated!`,
+            Course: updated
         });
+    } catch ( error ) {
+        return next(error);
     }
-
-    const updated = await coursesService.updateCourse ( id, { course_name, course_location, holes, par } );
-
-    if ( !updated ) {
-        return res.status(404).json({
-            success: false,
-            message: `Course(${id}) does not exist!`
-        });
-    }
-
-    return res.status(200).json({
-        success: true,
-        message: `Course updated!`,
-        Course: updated
-    })
-}
+};
 
 export default { getAllCourses, getCourseById, createCourse, deleteCourse, updatedCourse };
