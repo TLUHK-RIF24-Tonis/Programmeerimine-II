5de41a1605127ddc673f588b42b53f65ab3edb20
Tõnis P <putsepp.tonis@gmail.com>
2025-11-25 03:26:04 +0200

    chore: routes order and documentation update

--- API_Development/code/src/discs/discsRouter.ts
+++ API_Development/code/src/discs/discsRouter.ts
@@ -6,11 +6,11 @@ const router = Router();
 
 router.get('/', discsController.getAllDiscs);
 router.get('/:id', discsController.getDiscById);
+router.get('/me', discsController.getMyDiscs);
 router.post('/', isAdmin, discsController.createDisc);
 router.get("/user/:id", isAdmin, discsController.getUserDiscs);
 router.get("/user/disc/check", isAdmin, discsController.userHaveDisc);
 router.delete('/:id', isAdmin, discsController.deleteDisc);
-router.get('/me', discsController.getMyDiscs);
 router.patch('/:id', isAdmin, discsController.updateDisc)
 
 export default router;
